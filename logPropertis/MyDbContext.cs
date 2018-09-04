92b7cb8f1454d5ee56ab4852c7a09c0fee8b745e
siasty <siasty3@gmail.com>
2018-09-04 14:04:46 +0200

    SaveChanges

--- logPropertis/MyDbContext.cs
+++ logPropertis/MyDbContext.cs
@@ -18,7 +18,43 @@ namespace logPropertis
             optionsBuilder.UseNpgsql("User ID=postgres;Password=Th9S38vk;Server=localhost;Port=5432;Database=dbWebTest;Integrated Security=true;Pooling=true;");
         }
 
+    
         public DbSet<Test> Test { get; set; }
+        public virtual DbSet<ChangeLog> ChangeLogs { get; set; }
+
+        public override int SaveChanges()
+        {
+            var modifiedEntities = ChangeTracker.Entries()
+                .Where(p => p.State == EntityState.Modified).ToList();
+            var now = DateTime.Now;
+
+            foreach (var change in modifiedEntities)
+            {
+                var entityName = change.Entity.GetType().Name;
+
+                foreach (var prop in change.OriginalValues.Properties)
+                {
+                    var originalValue = change.OriginalValues[prop].ToString();
+                    var currentValue  = change.CurrentValues[prop].ToString();
+                    if (originalValue != currentValue)
+                    {
+                        ChangeLog log = new ChangeLog()
+                        {
+                            TableName = entityName,
+                            ColumnName = prop.Name,
+                            OldValue = originalValue,
+                            NewValue = currentValue,
+                            DateChanged = now
+                        };
+                        ChangeLogs.Add(log);
+                    }
+                }
+            }
+            return base.SaveChanges();
+        }
+
+
+
 
     }
 }
